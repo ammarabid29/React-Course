561c72077463afa5d1feb7e66f0f73a8fde1157c
ammarabid29 <aammarawan786@gmail.com>
2024-10-29 15:56:02 +0500

    setState hook

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+    if (counter == 20) {
+      setCounter(counter);
+    } else {
+      setCounter(counter + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if (counter == 0) {
+      setCounter(counter);
+    } else {
+      setCounter(counter - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
